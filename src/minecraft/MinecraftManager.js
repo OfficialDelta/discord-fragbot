397d8dd6437e634e9eb419eabaa6a583e676a1e6
TSLARoadster <51007646+TSLARoadster@users.noreply.github.com>
2021-07-05 14:22:42 -0400

    fixed 'can't read property bot of undefined'

--- src/minecraft/MinecraftManager.js
+++ src/minecraft/MinecraftManager.js
@@ -41,20 +41,21 @@ class MinecraftManager extends CommunicationBridge {
 	startFragBot() {
 		let DB = require('./jsonPull')
 		let partystatus = 'disbanded'
+		const bot = this.bot
 
-		this.bot.addChatPatternSet('GUILD_MESSAGE', [/^Guild > (?:\[(.+\+?\+?)\] )?(.+): (.+)$/], {
+		bot.addChatPatternSet('GUILD_MESSAGE', [/^Guild > (?:\[(.+\+?\+?)\] )?(.+): (.+)$/], {
 			parse: true,
 		})
 
-		this.bot.addChatPatternSet('PRIVATE_MESSAGE', [/^From (?:\[(.+\+?\+?)\] )?(.+): (.+)$/], {
+		bot.addChatPatternSet('PRIVATE_MESSAGE', [/^From (?:\[(.+\+?\+?)\] )?(.+): (.+)$/], {
 			parse: true,
 		})
 
-		this.bot.addChatPatternSet('PARTY_ACCEPT', [/^(?:\[(.+\+?\+?)\] )?(.+) joined the party./], {
+		bot.addChatPatternSet('PARTY_ACCEPT', [/^(?:\[(.+\+?\+?)\] )?(.+) joined the party./], {
 			parse: true,
 		})
 
-		this.bot.addChatPatternSet(
+		bot.addChatPatternSet(
 			'PARTY_INVITE',
 			[
 				/^-----------------------------\n(?:\[(.+\+?\+?)\] )?(.+) has invited you to join their party!\nYou have 60 seconds to accept. Click here to join!\n-----------------------------/,
@@ -64,7 +65,7 @@ class MinecraftManager extends CommunicationBridge {
 			}
 		)
 
-		this.bot.addChatPatternSet('PARTY_WARP', [/^Party Leader, (?:\[(.+\+?\+?)\] )?(.+), summoned you to their server./], {
+		bot.addChatPatternSet('PARTY_WARP', [/^Party Leader, (?:\[(.+\+?\+?)\] )?(.+), summoned you to their server./], {
 			parse: true,
 		})
 
@@ -76,39 +77,39 @@ class MinecraftManager extends CommunicationBridge {
 		// debuger
 
 		if (config.debug == 'on') {
-			this.bot.on('message', jsonMsg => {
+			bot.on('message', jsonMsg => {
 				console.log(`Debug > ${jsonMsg.toString()}`)
 			})
 		}
 
 		function leaveparty() {
-			this.bot.chat(`/p leave`)
+			bot.chat(`/p leave`)
 			console.log(`Log > Left the party`)
 			partystatus = 'disbanded'
 		}
 		function returntohub() {
 			waity(5)
 			console.log(`Log > Returnung to limbo`)
-			this.bot.chat(`/achat §c`)
+			bot.chat(`/achat §c`)
 		}
 
-		this.bot.on('chat:PARTY_WARP', ([[rank, username]]) => {
-			if (this.bot.username == username) {
+		bot.on('chat:PARTY_WARP', ([[rank, username]]) => {
+			if (bot.username == username) {
 				return
 			}
 			if (!rank) {
 				rank = 'Non'
 			}
 			waity(0.2)
-			this.bot.chat(`/msg ${username} ${config.warpmessage}`)
+			bot.chat(`/msg ${username} ${config.warpmessage}`)
 			console.log(`Log > ${username} tried to warp the party`)
 			waity(0.2)
 			leaveparty()
 			returntohub()
 		})
 
-		this.bot.on('chat:PARTY_INVITE', ([[rank, username]]) => {
-			if (this.bot.username == username) {
+		bot.on('chat:PARTY_INVITE', ([[rank, username]]) => {
+			if (bot.username == username) {
 				return
 			}
 			if (!rank) {
@@ -119,31 +120,31 @@ class MinecraftManager extends CommunicationBridge {
 				if (partystatus == 'busy') {
 					waity(0.2)
 					console.log(`Log > [${rank}] ${username} sent me a party invite but I was busy`)
-					this.bot.chat(`/msg ${username} Hi ${username}. I am currently in a party, please try again in a moment.`)
+					bot.chat(`/msg ${username} Hi ${username}. I am currently in a party, please try again in a moment.`)
 				} else {
 					console.log(`Log > [${rank}] ${username} sent me a party invite`)
-					this.bot.chat(`/p accept ${username}`)
+					bot.chat(`/p accept ${username}`)
 					partystatus = 'busy'
 					console.log(`Log > I accepted the invite`)
 					waity(0.2)
 					console.log(`Log > Begin intro`)
-					this.bot.chat(`/pc ${config.intro1}`)
+					bot.chat(`/pc ${config.intro1}`)
 					waity(0.2)
-					this.bot.chat(`/pc ${config.intro2}`)
+					bot.chat(`/pc ${config.intro2}`)
 					console.log(`Log > End intro`)
 					waity(0.5)
-					this.bot.chat(`/pc I will be leaving this party in ${config.timeinparty} seconds...`)
+					bot.chat(`/pc I will be leaving this party in ${config.timeinparty} seconds...`)
 					setTimeout(leaveparty, config.timeinparty * 100)
 					returntohub()
 				}
 			} else {
 				waity(0.2)
-				this.bot.chat(`/msg ${username} You are not whitelisted, please contact ${config.owner} if you think this is an error.`)
+				bot.chat(`/msg ${username} You are not whitelisted, please contact ${config.owner} if you think this is an error.`)
 				return
 			}
 		})
 
-		this.bot.on('chat:GUILD_MESSAGE', ([[rank, username, message]]) => {
+		bot.on('chat:GUILD_MESSAGE', ([[rank, username, message]]) => {
 			if (!rank) {
 				rank = 'Non'
 			}
@@ -152,40 +153,40 @@ class MinecraftManager extends CommunicationBridge {
 			// console.log(`Guild > [${rank}] ${username}: ${message}`);
 
 			if (message == `${config.prefix} ireadthecode`) {
-				this.bot.chat(`/msg ${username} you sure did!`)
+				bot.chat(`/msg ${username} you sure did!`)
 				console.log(`Log > ${username} read the code!`)
 			}
 
 			if (message == `${config.prefix} help`) {
 				if (username.toLowerCase() == config.owner.toLowerCase()) {
 					console.log(`Log > ${username} asked for owner help`)
-					this.bot.chat(`/gc ----------Commands----------- "`)
+					bot.chat(`/gc ----------Commands----------- "`)
 					waity(0.1)
-					this.bot.chat(`/gc ${config.prefix} whitelist me: whitelist yourself.`)
+					bot.chat(`/gc ${config.prefix} whitelist me: whitelist yourself.`)
 					waity(0.1)
-					this.bot.chat(`/gc ${config.prefix} blacklist <player>: removes the given player from the whitelist`)
+					bot.chat(`/gc ${config.prefix} blacklist <player>: removes the given player from the whitelist`)
 					waity(0.1)
-					this.bot.chat(`/gc ${config.prefix} whitelist <player>: adds the given player to the whitelist`)
+					bot.chat(`/gc ${config.prefix} whitelist <player>: adds the given player to the whitelist`)
 					waity(0.1)
-					this.bot.chat(`/gc -----------------------------`)
+					bot.chat(`/gc -----------------------------`)
 				} else {
-					this.bot.chat(`/gc ----------Commands-----------`)
+					bot.chat(`/gc ----------Commands-----------`)
 					waity(0.1)
-					this.bot.chat(`/gc ${config.prefix} whitelist me: whitelist yourself.`)
+					bot.chat(`/gc ${config.prefix} whitelist me: whitelist yourself.`)
 					waity(0.1)
-					this.bot.chat(`/gc -----------------------------`)
+					bot.chat(`/gc -----------------------------`)
 					console.log(`Log > ${username} asked for help`)
 				}
 			}
 
 			if (message == `${config.prefix} whitelist me`) {
 				if (DB.GetUser(username)) {
-					this.bot.chat(`/gc ${username} is already whitelisted`)
+					bot.chat(`/gc ${username} is already whitelisted`)
 					console.log(`Log > ${username} asked to be whitelisted but already was`)
 					return
 				} else {
 					DB.AddUser(username.toLowerCase(), 'w')
-					this.bot.chat(`/gc ${username} is now whitelisted`)
+					bot.chat(`/gc ${username} is now whitelisted`)
 					console.log(`Log > ${username} asked to be whitelisted and was`)
 				}
 			} else {
@@ -194,13 +195,13 @@ class MinecraftManager extends CommunicationBridge {
 						message = message.replace(/^[^\s]+./, '')
 						message = message.replace(/whitelist /, '').toLowerCase()
 						if (DB.GetUser(message)) {
-							this.bot.chat(`/gc ${message} is already whitelisted`)
+							bot.chat(`/gc ${message} is already whitelisted`)
 							console.log(`Log > ${config.owner} asked for ${message} to be whitelisted but they already were`)
 							return
 						} else {
 							DB.AddUser(message.replace(/${prefix} whitelist /, '').toLowerCase(), 'w')
 							console.log(`Log > ${config.owner} asked for ${message} to be whitelisted and they were`)
-							this.bot.chat(`/gc ${message} is now whitelisted`)
+							bot.chat(`/gc ${message} is now whitelisted`)
 						}
 					}
 				}
@@ -212,10 +213,10 @@ class MinecraftManager extends CommunicationBridge {
 					message = message.replace(/blacklist /, '').toLowerCase()
 					if (DB.GetUser(message)) {
 						DB.RemoveUser(message).toLowerCase()
-						this.bot.chat(`/gc ${message} is now blacklisted`)
+						bot.chat(`/gc ${message} is now blacklisted`)
 						console.log(`Log > ${config.owner} asked for ${message} to be blacklisted and they were`)
 					} else {
-						this.bot.chat(`/gc ${message} is already blacklisted`)
+						bot.chat(`/gc ${message} is already blacklisted`)
 						console.log(`Log > ${config.owner} asked for ${message} to be blacklisted but they already were`)
 						return
 					}
@@ -223,11 +224,11 @@ class MinecraftManager extends CommunicationBridge {
 			}
 		})
 
-		this.bot.on('spawn', () => {
+		bot.on('spawn', () => {
 			returntohub()
 		})
 
-		this.bot.on('error', err => console.log(err))
+		bot.on('error', err => console.log(err))
 	}
 
 	onBroadcast({ username, message, replyingTo }) {
